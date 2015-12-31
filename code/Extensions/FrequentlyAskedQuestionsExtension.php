4173a3c8ef7f20eea7ca494b068016308ce3f30f
helpfulrobot <chris@silverstripe.com>
2016-01-01 01:36:39 +1300

    Converted to PSR-2

--- code/Extensions/FrequentlyAskedQuestionsExtension.php
+++ code/Extensions/FrequentlyAskedQuestionsExtension.php
@@ -1,47 +1,48 @@
 <?php
 
-class FrequentlyAskedQuestionsExtension extends SiteTreeExtension {
+class FrequentlyAskedQuestionsExtension extends SiteTreeExtension
+{
 
-	private static $has_many = [
-		'Faqs' => 'FaqSegment'
-	];
+    private static $has_many = [
+        'Faqs' => 'FaqSegment'
+    ];
 
-	public function updateCMSFields(FieldList $fields) {
+    public function updateCMSFields(FieldList $fields)
+    {
 
-		/** @var GridFieldConfig $gridConfig */
-		$gridConfig = GridFieldConfig::create();
+        /** @var GridFieldConfig $gridConfig */
+        $gridConfig = GridFieldConfig::create();
 
-		$gridConfig
-			->addComponent(new GridFieldButtonRow('before'))
-			->addComponent(new GridFieldAddNewButton('buttons-before-left'))
-			->addComponent(new GridFieldToolbarHeader())
-			->addComponent(new  GridFieldSortableHeader())
-			->addComponent(new GridFieldSortableRows('SortOrder'))
-			->addComponent($dataColumns = new GridFieldDataColumns())
-			->addComponent(new GridFieldEditButton())
-			->addComponent(new GridFieldDeleteAction())
-			->addComponent(new GridFieldDetailForm());
+        $gridConfig
+            ->addComponent(new GridFieldButtonRow('before'))
+            ->addComponent(new GridFieldAddNewButton('buttons-before-left'))
+            ->addComponent(new GridFieldToolbarHeader())
+            ->addComponent(new  GridFieldSortableHeader())
+            ->addComponent(new GridFieldSortableRows('SortOrder'))
+            ->addComponent($dataColumns = new GridFieldDataColumns())
+            ->addComponent(new GridFieldEditButton())
+            ->addComponent(new GridFieldDeleteAction())
+            ->addComponent(new GridFieldDetailForm());
 
-		$dataColumns->setDisplayFields([
-			'Title'          => 'Question',
-			'Answer.Summary' => 'Answer Preview',
-		]);
-		
-		/** @var TabSet $rootTab */
-        	//We need to repush Metadata to ensure it is the last tab
-        	$rootTab = $fields->fieldByName('Root');
-        	$rootTab->push(Tab::create('FaqSegments'));
-        	if ($rootTab->fieldByName('Metadata')) {
-            		$metaChildren = $rootTab->fieldByName('Metadata')->getChildren();
-            		$rootTab->removeByName('Metadata');
-            		$rootTab->push(Tab::create('Metadata')->setChildren($metaChildren));
-        	}
+        $dataColumns->setDisplayFields([
+            'Title'          => 'Question',
+            'Answer.Summary' => 'Answer Preview',
+        ]);
+        
+        /** @var TabSet $rootTab */
+            //We need to repush Metadata to ensure it is the last tab
+            $rootTab = $fields->fieldByName('Root');
+        $rootTab->push(Tab::create('FaqSegments'));
+        if ($rootTab->fieldByName('Metadata')) {
+            $metaChildren = $rootTab->fieldByName('Metadata')->getChildren();
+            $rootTab->removeByName('Metadata');
+            $rootTab->push(Tab::create('Metadata')->setChildren($metaChildren));
+        }
 
-		$GridField = GridField::create('FaqSegments', 'FAQs', $this->owner->Faqs(), $gridConfig);
+        $GridField = GridField::create('FaqSegments', 'FAQs', $this->owner->Faqs(), $gridConfig);
 
-		$fields->addFieldToTab('Root.FaqSegments', $GridField);
+        $fields->addFieldToTab('Root.FaqSegments', $GridField);
 
-		return $fields;
-	}
-
+        return $fields;
+    }
 }
