f16a994c61a8e01cd45b6b2eca21de3a70bb9530
Otis Wright <otis@webfox.co.nz>
2016-11-21 13:27:29 +1300

    Update FrequentlyAskedQuestionsExtension.php

--- code/Extensions/FrequentlyAskedQuestionsExtension.php
+++ code/Extensions/FrequentlyAskedQuestionsExtension.php
@@ -18,30 +18,29 @@ class FrequentlyAskedQuestionsExtension extends SiteTreeExtension
             ->addComponent(new GridFieldAddNewButton('buttons-before-left'))
             ->addComponent(new GridFieldToolbarHeader())
             ->addComponent(new  GridFieldSortableHeader())
-            ->addComponent(new GridFieldSortableRows('SortOrder'))
+            ->addComponent(new GridFieldOrderableRows('SortOrder'))
             ->addComponent($dataColumns = new GridFieldDataColumns())
             ->addComponent(new GridFieldEditButton())
             ->addComponent(new GridFieldDeleteAction())
             ->addComponent(new GridFieldDetailForm());
 
         $dataColumns->setDisplayFields([
-            'Title'          => 'Question',
+            'Title' => 'Question',
             'Answer.Summary' => 'Answer Preview',
         ]);
-        
+
         /** @var TabSet $rootTab */
-            //We need to repush Metadata to ensure it is the last tab
-            $rootTab = $fields->fieldByName('Root');
-        $rootTab->push(Tab::create('FaqSegments'));
-        if ($rootTab->fieldByName('Metadata')) {
-            $metaChildren = $rootTab->fieldByName('Metadata')->getChildren();
-            $rootTab->removeByName('Metadata');
-            $rootTab->push(Tab::create('Metadata')->setChildren($metaChildren));
+        //We need to repush Metadata to ensure it is the last tab
+        $rootTab = $fields->fieldByName('Root');
+        if ($this->owner->exists()) {
+            $rootTab->push(Tab::create('FaqSegments'));
         }
 
         $GridField = GridField::create('FaqSegments', 'FAQs', $this->owner->Faqs(), $gridConfig);
 
-        $fields->addFieldToTab('Root.FaqSegments', $GridField);
+        if ($this->owner->exists()) {
+            $fields->addFieldToTab('Root.FaqSegments', $GridField);
+        }
         $fields->removeByName('Faqs');
         return $fields;
     }
